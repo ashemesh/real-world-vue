1044f50788bef5b4cb4f918ad034ddefae480231
Assaf Shemesh <7108assafshe@gmail.com>
2019-04-01 20:47:35 +0200

    done w/ lesson #8

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -7,7 +7,7 @@
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
       <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
+      <span name="users">{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
 </template>
